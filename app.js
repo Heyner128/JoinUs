777d90ea641a49594241f03f049fe1f295b116f0
Heyner Cuevas <heyner128@gmail.com>
2022-03-12 22:33:03 +0100

    heroku things

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const client = new Client();
 (async function() {await client.connect()})();
 const bodyParser = require('body-parser');
-const PORT - process.env.port || 5000;
+const PORT = process.env.port || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', "ejs");
